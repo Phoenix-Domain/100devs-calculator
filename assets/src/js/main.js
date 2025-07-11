c3e70a228fd96280c77ff6a891f5898e77f238f6
Batubo Victory <victorybatubo76@gmail.com>
2025-07-11 13:12:24 +0100

    Unit test #1

---
+++ assets/src/js/main.js
@@ -0,0 +1,27 @@
+const result = document.querySelector('#result');
+const no7 = document.querySelector('#no7');
+const no8 = document.querySelector('#no8');
+const no9 = document.querySelector('#no9');
+const no6 = document.querySelector('#no6');
+const no5 = document.querySelector('#no5');
+const no4 = document.querySelector('#no4');
+const no3 = document.querySelector('#no3');
+const no2 = document.querySelector('#no2');
+const no1 = document.querySelector('#no1');
+const no0 = document.querySelector('#no0');
+const additionOperator = document.querySelector('#additionOperator');
+const minusOperator = document.querySelector('#minusOperator');
+const multiplicationOperator = document.querySelector(' multiplicationOperator');
+const divisionOperator = document.querySelector('#divisionOperator');
+const decimalOperator = document.querySelector('#decimalOperator');
+
+no7.addEventListener('click', function(){
+  result.textContent = "7"; 
+});
+//User clicks button
+
+//Value gets updated to the DOM
+
+//Evaluates the calculation
+
+//Displays the result
